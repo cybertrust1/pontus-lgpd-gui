f5da0a6903d562a195ffb061f8f5be18deaab798
Leonardo Martins <lmartins@pontusnetworks.com>
2019-06-20 17:49:27 +0100

    fixed output of track expert view

--- src/PVAceGremlinJSONQueryResults.js
+++ src/PVAceGremlinJSONQueryResults.js
@@ -101,12 +101,21 @@ class PVAceGremlinJSONQueryResults extends PontusComponent
   {
     // let eventHub = this.props.glEventHub;
     //
-    
-    let data = this.state.value;
-    if (typeof this.state.value === "object")
+  
+    let data = (this.state.value);
+    if (data.response){
+      data = data.response;
+    }
+    if (data.data){
+      data = data.data;
+    }
+    if (data.result){
+      data = data.result;
+    }
+    if (typeof data === "object")
     {
       data = JSON.stringify(data, null, 2);
-      
+    
     }
     data = data.replace(/\\n/g, '\n');
     data = data.replace(/\\t/g, '\t');
